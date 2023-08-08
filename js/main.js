725227bc554b6c3c7671d95530315983f87c62cc
athen <ayoslinthen@gmail.com>
2023-08-07 23:40:01 -0400

    Add scroll back to top functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-//scroll to top
+/* Back to top button */
+const topButton = document.getElementById("backTop");
+// show only when user scrolls below fold of the page.
+window.onscroll = function scrollFunction() {
+ if (document.documentElement.scrollTop > 800) {
+  topButton.style.display = "block";
+ } else {
+  topButton.style.display = "none";
+ }
+}//scroll to top
 const topFunction = () =>
-    document.documentElement.scrollTo({ top: 0, behavior: 'smooth' });
+ document.documentElement.scrollTo({ top: 0, behavior: 'smooth' });
